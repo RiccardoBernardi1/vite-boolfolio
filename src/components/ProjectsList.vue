a52f11af06cf06a23132a579e898385de9cd9a4a
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-02-01 14:59:00 +0100

    Aggiunge lo store.js e sposta la chiamata axios nella App.vue

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,20 +1,15 @@
 <script>
+import { store } from "../store";
 import ProjectCard from "./ProjectCard.vue";
-import axios from "axios";
 export default {
   name: "ProjectsList",
-  data() {
-    return {
-      projects: [],
-    };
-  },
   components: {
     ProjectCard,
   },
-  created() {
-    axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
-      this.projects = response.data;
-    });
+  data() {
+    return {
+      store,
+    };
   },
 };
 </script>
@@ -23,7 +18,7 @@ export default {
   <section>
     <h2 class="my-4">Projects List</h2>
     <div class="row g-4">
-      <div class="col-4" v-for="project in projects">
+      <div class="col-4" v-for="project in store.projects">
         <ProjectCard :info="project" />
       </div>
     </div>
